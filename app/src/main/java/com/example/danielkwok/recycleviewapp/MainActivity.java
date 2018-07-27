c463fc78bee1c9d1a471048eb738093207faa246
danielkwok21 <danielzhengxian21@gmail.com>
2018-07-27 15:38:51 +0800

    cleaned up irrelevant code

--- app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
+++ app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
@@ -2,11 +2,6 @@ package com.example.danielkwok.recycleviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
