942d40b4fdf442595b0d9c14ac824dd9e96337db
danielkwok21 <danielzhengxian21@gmail.com>
2018-07-27 16:24:57 +0800

    empty toolbar set

--- app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
+++ app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.danielkwok.recycleviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    //using toolbar with android.v7.widget
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolBar);
+
+        //set toolbar as default action bar
+        //as a result, the name of the activity is shown on this toolbar
+        setSupportActionBar(toolbar);
     }
 }
