dca4730e9a6e6e808ee08abfdec9e8dc7979951c
danielkwok21 <danielzhengxian21@gmail.com>
2018-08-12 22:54:35 +0800

    Person class added

--- app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
+++ app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
@@ -3,23 +3,48 @@ package com.example.danielkwok.recycleviewapp;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
     TextView nameLabel;
     EditText nameInput;
     TextView ageLabel;
     EditText ageInput;
+    Button enterButton;
+
+    Person p;
+    String name;
+    int age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        nameLabel = findViewById(R.id.ageLabel);
+        nameLabel = findViewById(R.id.nameLabel);
         nameInput = findViewById(R.id.nameInput);
         ageLabel = findViewById(R.id.ageLabel);
         ageInput = findViewById(R.id.ageInput);
+        enterButton = findViewById(R.id.enterButton);
+
+        enterButton.setOnClickListener((View v)->{
+            name = nameInput.getText().toString();
+            try{
+                age = Integer.parseInt(ageInput.getText().toString());
+            }catch(NumberFormatException e){
+                Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show();
+            }
+            p = new Person(name, age);
+            Log.d(TAG, p.toJSON());
+
+            Toast.makeText(this, "entered new person", Toast.LENGTH_LONG).show();
+        });
     }
+
 }
