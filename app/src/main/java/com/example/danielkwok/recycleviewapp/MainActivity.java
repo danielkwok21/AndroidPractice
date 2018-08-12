052a5114237aea7965b59c0f25ddb2f87d6e3dbe
danielkwok21 <danielzhengxian21@gmail.com>
2018-08-12 11:06:57 +0800

    created simple input form

--- app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
+++ app/src/main/java/com/example/danielkwok/recycleviewapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.danielkwok.recycleviewapp;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView nameLabel;
+    EditText nameInput;
+    TextView ageLabel;
+    EditText ageInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nameLabel = findViewById(R.id.ageLabel);
+        nameInput = findViewById(R.id.nameInput);
+        ageLabel = findViewById(R.id.ageLabel);
+        ageInput = findViewById(R.id.ageInput);
     }
 }
